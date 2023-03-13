b75206dd4dc22cba24db6da84aa582497326c882
Siarheimasiuk <winst73@gmail.com>
2023-03-13 20:26:43 +0300

    setup project and start

---
+++ TestTask/Cordinator/Cordinators/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  TestTask
+//
+//  Created by Sergio on 13.03.23.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var parentCoordinator: Coordinator? { get set }
+    var children: [Coordinator] { get set }
+    var navigationController : UINavigationController { get set }
+
+    func start()
+    
+}
---
+++ TestTask/ViewModel/RegisterViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewModel.swift
+//  TestTask
+//
+//  Created by Sergio on 13.03.23.
+//
+
+import Foundation
+
+class RegisterViewModel {
+
+    weak var appCoordinator : AppCoordinator!
+
+    func goToLogin() {
+        appCoordinator.goToLoginPage()
+    }
+}
